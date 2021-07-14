fix kl computation within elbo loss
Signed-off-by: Ranganath Krishnan <ranganath.krishnan@intel.com>

bayesian_torch/examples/main_bayesian_cifar.py
@@ -115,7 +115,7 @@ parser.add_argument(
     help='number of Monte Carlo samples to be drawn during inference')
 parser.add_argument('--num_mc',
                     type=int,
-                    default=5,
+                    default=1,
                     metavar='N',
                     help='number of Monte Carlo runs during training')
 parser.add_argument(
@@ -367,22 +367,9 @@ def train(args,
         output = torch.mean(torch.stack(output_), dim=0)
         kl = torch.mean(torch.stack(kl_), dim=0)
         cross_entropy_loss = criterion(output, target_var)
-        scaled_kl = kl / len_trainset
+        scaled_kl = kl / args.batch_size 
         #ELBO loss
         loss = cross_entropy_loss + scaled_kl
-        '''
-        #another way of computing gradients with multiple MC samples
-        cross_entropy_loss = 0
-        scaled_kl = 0
-        for mc_run in range(args.num_mc):
-            output, kl = model(input_var)
-            cross_entropy_loss += criterion(output, target_var)
-            scaled_kl += (kl/len_trainset)
-        cross_entropy_loss = cross_entropy_loss/args.num_mc
-        scaled_kl = scaled_kl/args.num_mc
-        loss = cross_entropy_loss + scaled_kl
-        #end
-        '''
 
         # compute gradient and do SGD step
         optimizer.zero_grad()
@@ -456,7 +443,7 @@ def validate(args, val_loader, model, criterion, epoch, tb_writer=None):
             output = torch.mean(torch.stack(output_), dim=0)
             kl = torch.mean(torch.stack(kl_), dim=0)
             cross_entropy_loss = criterion(output, target_var)
-            scaled_kl = kl / len_trainset
+            scaled_kl = kl / args.batch_size 
             #ELBO loss
             loss = cross_entropy_loss + scaled_kl
 

bayesian_torch/examples/main_bayesian_mnist.py
@@ -32,18 +32,7 @@ def train(args, model, device, train_loader, optimizer, epoch, tb_writer=None):
         kl = torch.mean(torch.stack(kl_), dim=0)
         nll_loss = F.nll_loss(output, target)
         #ELBO loss
-        loss = nll_loss + (kl / len_trainset)
-        '''
-        #another way of computing gradients with multiple MC samples
-        nll_loss = 0
-        scaled_kl = 0
-        for mc_run in range(args.num_mc):
-            output, kl = model(data)
-            nll_loss += F.nll_loss(output, target)
-            scaled_kl += (kl/len_trainset)
-        loss = (nll_loss + scaled_kl)/args.num_mc
-        #end
-        '''
+        loss = nll_loss + (kl / args.batch_size)
 
         loss.backward()
         optimizer.step()
@@ -67,7 +56,7 @@ def test(args, model, device, test_loader, epoch, tb_writer=None):
             data, target = data.to(device), target.to(device)
             output, kl = model(data)
             test_loss += F.nll_loss(output, target, reduction='sum').item() + (
-                kl / len_testset)  # sum up batch loss
+                kl / args.batch_size)  # sum up batch loss
             pred = output.argmax(
                 dim=1,
                 keepdim=True)  # get the index of the max log-probability
@@ -165,7 +154,7 @@ def main():
         help='number of Monte Carlo samples to be drawn for inference')
     parser.add_argument('--num_mc',
                         type=int,
-                        default=5,
+                        default=1,
                         metavar='N',
                         help='number of Monte Carlo runs during training')
     parser.add_argument(

bayesian_torch/layers/base_variational_layer.py
@@ -50,4 +50,4 @@ class BaseVariationalLayer_(nn.Module):
         kl = torch.log(sigma_p) - torch.log(
             sigma_q) + (sigma_q**2 + (mu_q - mu_p)**2) / (2 *
                                                           (sigma_p**2)) - 0.5
-        return kl.sum()
+        return kl.mean()