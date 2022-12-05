3ea51a585d49b6e057f817a85629d141c51f52a1
junliang-lin <jun-liang.lin@intel.com>
2022-12-04 23:40:35 -0500

    quantized conv flipout layers

--- bayesian_torch/layers/flipout_layers/__init__.py
+++ bayesian_torch/layers/flipout_layers/__init__.py
@@ -1,3 +1,6 @@
 from .conv_flipout import *
 from .linear_flipout import *
 from .rnn_flipout import *
+# from .quantized_linear_flipout import *
+from .quantized_conv_flipout import *
+# from .quantize_rnn_flipout import *
